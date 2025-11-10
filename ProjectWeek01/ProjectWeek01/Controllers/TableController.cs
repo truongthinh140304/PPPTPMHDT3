8506971fed1ab5b302639c19e4bf04e73eca08d6
thinh <truongthinhng14@gmail.com>
2025-11-10 15:33:53 +0700

    update

---
+++ ProjectWeek01/ProjectWeek01/Controllers/TableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWeek01.Controllers
+{
+    public class TableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
